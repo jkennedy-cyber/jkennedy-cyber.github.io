5be6e12210e23a28124779afc0f21e981408a6cc
jkennedy-cyber <jckennedy@my.waketech.edu>
2025-07-09 15:40:29 -0400

    had to rename

---
+++ gradebook.js
@@ -0,0 +1,10 @@
+function fetchGradebook() {
+    console.log("Fetching grade data...");
+}
+
+function populateGradebook(data) {
+    console.log("Populating gradebook with data:", data);
+}
+
+const gradeData = fetchGradebook();
+populateGradebook(gradeData);
